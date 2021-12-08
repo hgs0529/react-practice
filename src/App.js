1648fd350aacef3d7410f8f7bc71f1cb184e95d6
gilsunghwang <gilsung-hwang@hwang-gilseong-ui-MacBookAir.local>
2021-12-08 16:13:41 +0900

    styled-components practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+// const Father = styled.div`
+//   display: flex;
+// `;
+
+// const Box = styled.div`
+//   background-color: ${(props) => props.bgColor};
+//   width: 100px;
+//   height: 100px;
+// `;
+
+// const Circle = styled(Box)`
+//   border-radius: 50%;
+// `;
+
+// const Input = styled.input.attrs({ required: true })`
+//   background-color: tomato;
+// `;
+
+// const animation = keyframe`
+//   0% {
+//     transform: rotate(0deg);
+//     border-radius: 0px;
+//   }
+//   50% {
+//     border-radius: 100px;
+//   }
+//   100% {
+//     transform: rotate(360deg);
+//     border-radius: 0px;
+//   }
+// `;
+
+// const Emoji = styled.span`
+//   font-size: 36px;
+// `;
+
+// const Box = styled.div`
+//   height: 200px;
+//   width: 200px;
+//   background-color: tomato;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   animation: ${animation} 1s linear infinite;
+//   ${Emoji} {
+//     &:hover {
+//       font-size: 98px;
+//     }
+//   }
+// `;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>할로</Title>
+    </Wrapper>
   );
 }
 
